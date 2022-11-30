ce33ab19960bbedfe3af5f6dc6fb268411542d60
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-30 20:08:44 +0100

    Create actions for bookmark and tags

--- app/Http/Controllers/Bookmarks/StoreController.php
+++ app/Http/Controllers/Bookmarks/StoreController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Bookmarks;
 
+use App\Actions\Bookmarks\CreateBookmarkAndTags;
 use App\Models\Tag;
 use App\Models\Bookmark;
 use Illuminate\Support\Str;
@@ -15,20 +16,11 @@ class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request): RedirectResponse
     {
-        $bookmark = auth()->user()->bookmarks()->create([
-            'name' => $request->get('name'),
-            'url' => $request->get('url'),
-            'description' => $request->get('description'),
-        ]);
- 
-        foreach (explode(',', $request->get('tags')) as $tag) {
-            $tag = Tag::query()->firstOrCreate(
-                ['name' => trim(strtolower($tag))],
-            );
- 
-            $bookmark->tags()->attach($tag->id);
-        }
- 
-        return redirect()->route('dashboard');
+        (new CreateBookmarkAndTags())->handle(
+            request: $request->all(), 
+            id: auth()->id()
+        );
+
+        return to_route('dashboard');
     }
 }
