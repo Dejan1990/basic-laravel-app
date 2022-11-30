9704cf61b8e02f884e1ce2f1753c5b343770a51e
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-30 20:53:14 +0100

    Housekeeping

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post(
+        'bookmarks',
+        App\Http\Controllers\Bookmarks\StoreController::class,
+    )->name('bookmarks.store');
+    
+    Route::delete(
+        'bookmarks/{bookmark}',
+        App\Http\Controllers\Bookmarks\DeleteController::class,
+    )->name('bookmarks.delete');
+    
+    Route::get(
+        'bookmarks/{bookmark}',
+        App\Http\Controllers\Bookmarks\RedirectController::class
+    )->name('bookmarks.redirect');
 });
-
-Route::post(
-    'bookmarks',
-    App\Http\Controllers\Bookmarks\StoreController::class,
-)->middleware(['auth'])->name('bookmarks.store');
-
-Route::delete(
-    'bookmarks/{bookmark}',
-    App\Http\Controllers\Bookmarks\DeleteController::class,
-)->middleware(['auth'])->name('bookmarks.delete');
-
-Route::get(
-    'bookmarks/{bookmark}',
-    App\Http\Controllers\Bookmarks\RedirectController::class
-)->middleware(['auth'])->name('bookmarks.redirect');
 
 require __DIR__.'/auth.php';
