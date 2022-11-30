533bad766a7431d8e214fc4d12bd28f15367f70d
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-30 16:14:39 +0100

    Tag model and migration and create pivot table

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Bookmark extends Model
 {
@@ -18,6 +19,14 @@ class Bookmark extends Model
         return $this->belongsTo(
             related: User::class,
             foreignKey: 'user_id',
+        );
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            related: Tag::class,
+            table: 'bookmark_tag'
         );
     }
 }
